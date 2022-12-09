52d10556774a10cf41cb97fa4e98e2606cb3febd
Robert Buruleanu <robertionut95b@gmail.com>
2022-12-09 12:53:49 +0200

    [frontend] Add ROLE_RECRUITER as value in enum RoleType

--- px-ui/src/auth/permission.types.ts
+++ px-ui/src/auth/permission.types.ts
@@ -6,5 +6,6 @@ export enum PermissionType {
 export enum RoleType {
   ROLE_ADMINISTRATOR = "ROLE_ADMINISTRATOR",
   ROLE_VIEWER = "ROLE_VIEWER",
+  ROLE_RECRUITER = "ROLE_RECRUITER",
   ROLE_EVERYONE = "ROLE_EVERYONE",
 }

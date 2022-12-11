4760afe5f1de8a1edb24ac1659ac2f03a62d7f82
Robert Buruleanu <robertionut95b@gmail.com>
2022-12-11 09:17:31 +0200

    [backend] Fix auditable entity, add superclass annotation to include fields in all entities

--- src/main/java/com/irb/paxton/auditable/AuditableEntity.java
+++ src/main/java/com/irb/paxton/auditable/AuditableEntity.java
@@ -9,11 +9,13 @@ import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
-@EntityListeners(AuditingEntityListener.class)
 @Getter
 @Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class AuditableEntity {
 
     @CreatedBy

7c27ebc3e2588578b0d726c2d5ca4b563c649153
Robert Buruleanu <robertionut95b@gmail.com>
2022-12-14 14:53:31 +0200

    [frontend] Add vite eslint plugin

--- px-ui/vite.config.ts
+++ px-ui/vite.config.ts
@@ -1,10 +1,11 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import eslint from "vite-plugin-eslint";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), eslint()],
   server: {
     host: true,
     port: 3000,

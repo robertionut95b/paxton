0cc865241ec82f51d753ddfdc1062580afa82640
Robert Buruleanu <robertionut95b@gmail.com>
2022-12-19 09:26:04 +0200

    [backend] Create JobListing mapper utility

--- src/main/java/com/irb/paxton/core/jobs/JobListingService.java
+++ src/main/java/com/irb/paxton/core/jobs/JobListingService.java
@@ -1,23 +1,17 @@
 package com.irb.paxton.core.jobs;
 
-import com.irb.paxton.core.jobs.category.JobCategory;
-import com.irb.paxton.core.jobs.category.JobCategoryRepository;
-import com.irb.paxton.core.jobs.category.exception.JobCategoryNotExistsException;
-import com.irb.paxton.core.jobs.exception.JobNotExistsException;
 import com.irb.paxton.core.jobs.input.JobListingInput;
-import com.irb.paxton.core.location.City;
-import com.irb.paxton.core.location.CityRepository;
-import com.irb.paxton.core.organization.Organization;
-import com.irb.paxton.core.organization.OrganizationRepository;
-import com.irb.paxton.core.organization.exception.OrganizationNotExistsException;
+import com.irb.paxton.core.jobs.mapper.JobListingMapper;
 import com.irb.paxton.core.search.PaginatedResponse;
 import com.irb.paxton.core.search.SearchRequest;
 import com.irb.paxton.core.search.SearchSpecification;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
@@ -27,16 +21,7 @@ public class JobListingService {
     private JobListingRepository jobListingRepository;
 
     @Autowired
-    private JobService jobService;
-
-    @Autowired
-    private OrganizationRepository organizationRepository;
-
-    @Autowired
-    private JobCategoryRepository jobCategoryRepository;
-
-    @Autowired
-    private CityRepository cityRepository;
+    private JobListingMapper jobListingMapper;
 
     public List<JobListing> getAllJobListings() {
         return this.jobListingRepository.findAll();
@@ -55,22 +40,10 @@ public class JobListingService {
         );
     }
 
+    @Transactional
+    @PostAuthorize("hasRole('ROLE_ADMINISTRATOR') or hasRole('ROLE_RECRUITER') or @paxtonSecurityService.isOrganizationRecruiter(authentication, returnObject.organization)")
     public JobListing publishJobListing(JobListingInput jobListingInput) {
-
-        Job job = jobService.findById(jobListingInput.getJobId())
-                .orElseThrow(() -> new JobNotExistsException(String.format("Job by id %d does not exist", jobListingInput.getJobId()), "jobId"));
-        Organization organization = organizationRepository.findById(jobListingInput.getOrganizationId())
-                .orElseThrow(() -> new OrganizationNotExistsException(String.format("Organization by id %d does not exist", jobListingInput.getOrganizationId()), "organizationId"));
-        JobCategory jobCategory = jobCategoryRepository.findById(jobListingInput.getCategoryId())
-                .orElseThrow(() -> new JobCategoryNotExistsException(String.format("Job Category by id %d does not exist", jobListingInput.getCategoryId()), "categoryId"));
-        City city = this.cityRepository.findByName(jobListingInput.getLocation())
-                .orElseThrow(IllegalArgumentException::new);
-
-        JobListing jobListing = new JobListing(
-                jobListingInput.getTitle(), jobListingInput.getDescription(), jobListingInput.getAvailableFrom(),
-                jobListingInput.getAvailableTo(), false, city, jobListingInput.getNumberOfVacancies(),
-                job, jobListingInput.getContractType(), organization, jobCategory, null, null
-        );
+        JobListing jobListing = jobListingMapper.inputToJobListing(jobListingInput);
         jobListingRepository.save(jobListing);
 
         return jobListing;
---
+++ src/main/java/com/irb/paxton/core/jobs/mapper/JobListingMapper.java
@@ -0,0 +1,67 @@
+package com.irb.paxton.core.jobs.mapper;
+
+import com.irb.paxton.core.jobs.Job;
+import com.irb.paxton.core.jobs.JobListing;
+import com.irb.paxton.core.jobs.JobService;
+import com.irb.paxton.core.jobs.category.JobCategory;
+import com.irb.paxton.core.jobs.category.JobCategoryRepository;
+import com.irb.paxton.core.jobs.category.exception.JobCategoryNotExistsException;
+import com.irb.paxton.core.jobs.exception.JobNotExistsException;
+import com.irb.paxton.core.jobs.input.JobListingInput;
+import com.irb.paxton.core.location.City;
+import com.irb.paxton.core.location.CityRepository;
+import com.irb.paxton.core.organization.Organization;
+import com.irb.paxton.core.organization.OrganizationRepository;
+import com.irb.paxton.core.organization.exception.OrganizationNotExistsException;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Mapper(componentModel = "spring")
+public abstract class JobListingMapper {
+
+    @Autowired
+    private CityRepository cityRepository;
+
+    @Autowired
+    private JobService jobService;
+
+    @Autowired
+    private OrganizationRepository organizationRepository;
+
+    @Autowired
+    private JobCategoryRepository jobCategoryRepository;
+
+    @Mapping(target = "organization", source = "jobListingInput.organizationId")
+    @Mapping(target = "job", source = "jobListingInput.jobId")
+    @Mapping(target = "city", source = "jobListingInput.location")
+    @Mapping(target = "category", source = "jobListingInput.categoryId")
+    @Mapping(target = "process", ignore = true)
+    @Mapping(target = "modifiedBy", ignore = true)
+    @Mapping(target = "modifiedAt", ignore = true)
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdBy", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "applications", ignore = true)
+    @Mapping(target = "active", ignore = true)
+    public abstract JobListing inputToJobListing(JobListingInput jobListingInput);
+
+    public Organization mapOrganization(Long organizationId) {
+        return organizationRepository.findById(organizationId)
+                .orElseThrow(() -> new OrganizationNotExistsException(String.format("Organization by id %d does not exist", organizationId), "organizationId"));
+    }
+
+    public JobCategory mapJobCategory(Long categoryId) {
+        return jobCategoryRepository.findById(categoryId)
+                .orElseThrow(() -> new JobCategoryNotExistsException(String.format("Job Category by id %d does not exist", categoryId), "categoryId"));
+    }
+
+    public City mapCity(String location) {
+        return cityRepository.findByName(location).orElseThrow(IllegalArgumentException::new);
+    }
+
+    public Job mapJob(Long jobId) {
+        return jobService.findById(jobId)
+                .orElseThrow(() -> new JobNotExistsException(String.format("Job by id %d does not exist", jobId), "jobId"));
+    }
+}

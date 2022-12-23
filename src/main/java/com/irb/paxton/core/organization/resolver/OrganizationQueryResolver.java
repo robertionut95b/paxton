d9952ce69a4eafe5db32a3d4dd9eeaf64cfc9413
Robert Buruleanu <robertionut95b@gmail.com>
2022-12-23 12:14:11 +0200

    [backend] Add organization by id query resolver in gqlServer

--- src/main/java/com/irb/paxton/core/organization/resolver/OrganizationQueryResolver.java
+++ src/main/java/com/irb/paxton/core/organization/resolver/OrganizationQueryResolver.java
@@ -2,6 +2,7 @@ package com.irb.paxton.core.organization.resolver;
 
 import com.irb.paxton.core.organization.Organization;
 import com.irb.paxton.core.organization.OrganizationRepository;
+import com.irb.paxton.core.organization.exception.OrganizationNotExistsException;
 import graphql.kickstart.tools.GraphQLQueryResolver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -16,5 +17,10 @@ public class OrganizationQueryResolver implements GraphQLQueryResolver {
 
     public List<Organization> getAllOrganizations() {
         return organizationRepository.findAll();
+    }
+
+    public Organization getOrganizationById(Long organizationId) {
+        return organizationRepository.findById(organizationId)
+                .orElseThrow(() -> new OrganizationNotExistsException(String.format("Organization %s does not exist", organizationId)));
     }
 }

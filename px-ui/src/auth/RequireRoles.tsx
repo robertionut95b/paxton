0985af92bae5cb5148c28d099ed89a2052d6277c
Robert Buruleanu <robertionut95b@gmail.com>
2023-01-11 11:39:31 +0200

    [frontend] Add new components to handle user authorization

--- px-ui/src/auth/RequireRoles.tsx
+++ px-ui/src/auth/RequireRoles.tsx
@@ -1,28 +1,37 @@
-import { PermissionType, RoleType } from "@auth/permission.types";
+import { RoleType } from "@auth/permission.types";
 import { useAuth } from "@auth/useAuth";
 import { Navigate, useLocation } from "react-router-dom";
 
 const isAllowed = (
-  permission: PermissionType | string,
+  roles: RoleType | Array<RoleType> | string,
   permissions: string[]
-) =>
-  permissions
-    .map((p) => p.toLowerCase())
-    .includes(permission.toString().toLowerCase());
+) => {
+  if (Array.isArray(roles))
+    return permissions.some((p) =>
+      roles.some((r) => r.toLocaleLowerCase() === p.toLowerCase())
+    );
+  else
+    return permissions
+      .map((p) => p.toLowerCase())
+      .includes(roles.toString().toLowerCase());
+};
 
 export const isAdmin = (permissions: string[]) =>
   permissions.some((p) => p === RoleType.ROLE_ADMINISTRATOR);
 
+export const isRecruiter = (permissions: string[]) =>
+  permissions.some((p) => p === RoleType.ROLE_RECRUITER);
+
 type RequireProps = {
   children: JSX.Element;
-  permission: PermissionType | string | (() => boolean);
+  roles: Array<RoleType> | RoleType;
   strict?: boolean;
   returnValue?: "navigate" | "null";
 };
 
-export function RequirePermission({
+export function RequireRoles({
   children,
-  permission,
+  roles,
   strict = false,
   returnValue = "navigate",
 }: RequireProps) {
@@ -41,14 +50,8 @@ export function RequirePermission({
     if (isAdmin(permissions)) return children;
   }
 
-  if (typeof permission === "function") {
-    if (!permission()) {
-      return returnVal;
-    }
-  } else {
-    if (!isAllowed(permission, permissions)) {
-      return returnVal;
-    }
+  if (!isAllowed(roles, permissions)) {
+    return returnVal;
   }
 
   return children;

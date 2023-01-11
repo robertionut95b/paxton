f5304dadf1c82f6eb329682bb8639ad1e59794d2
Robert Buruleanu <robertionut95b@gmail.com>
2023-01-11 11:38:54 +0200

    [frontend] Optimize & add new rendering hooks

--- px-ui/src/hooks/useInterval.tsx
+++ px-ui/src/hooks/useInterval.tsx
@@ -1,18 +1,26 @@
+import useIsomorphicLayoutEffect from "@hooks/useIsomorphicLayoutEffect";
 import { useEffect, useRef } from "react";
 
-export default function useInterval(callback: () => void, delay: number) {
-  const savedCallback = useRef<() => void>();
+function useInterval(callback: () => void, delay: number | null) {
+  const savedCallback = useRef(callback);
 
-  useEffect(() => {
+  // Remember the latest callback if it changes.
+  useIsomorphicLayoutEffect(() => {
     savedCallback.current = callback;
-  });
+  }, [callback]);
 
+  // Set up the interval.
   useEffect(() => {
-    function tick() {
-      savedCallback.current?.();
+    // Don't schedule if no delay is specified.
+    // Note: 0 is a valid value for delay.
+    if (!delay && delay !== 0) {
+      return;
     }
 
-    const id = setInterval(tick, delay);
+    const id = setInterval(() => savedCallback.current(), delay);
+
     return () => clearInterval(id);
   }, [delay]);
 }
+
+export default useInterval;

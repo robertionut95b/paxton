6d2835f66c93f2e354740abfb460b89ec8c472c6
Robert Buruleanu <robertionut95b@gmail.com>
2023-03-10 16:27:05 +0200

    [backend] Comment out Exception.class handle method

--- src/main/java/com/irb/paxton/exceptions/handler/GlobalExceptionHandler.java
+++ src/main/java/com/irb/paxton/exceptions/handler/GlobalExceptionHandler.java
@@ -12,7 +12,6 @@ import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -80,14 +79,14 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         );
     }
 
-    @ExceptionHandler(Exception.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ResponseEntity<Object> handleDefaultException(Exception ex, WebRequest webRequest) {
-        log.error(ex.getMessage(), ex);
-        HttpStatus errorCode = HttpStatus.INTERNAL_SERVER_ERROR;
-        ApiErrorResponse apiErrorResponse = new ApiErrorResponse(errorCode.value(), "Internal error", "Unknown internal exception occurred");
-        return new ResponseEntity<>(
-                apiErrorResponse, new HttpHeaders(), apiErrorResponse.getStatus()
-        );
-    }
+//    @ExceptionHandler(Exception.class)
+//    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+//    public ResponseEntity<Object> handleDefaultException(Exception ex, WebRequest webRequest) {
+//        log.error(ex.getMessage(), ex);
+//        HttpStatus errorCode = HttpStatus.INTERNAL_SERVER_ERROR;
+//        ApiErrorResponse apiErrorResponse = new ApiErrorResponse(errorCode.value(), "Internal error", "Unknown internal exception occurred");
+//        return new ResponseEntity<>(
+//                apiErrorResponse, new HttpHeaders(), apiErrorResponse.getStatus()
+//        );
+//    }
 }

a73809e4801324e998458f620d801e1d52d53494
Robert Buruleanu <robertionut95b@gmail.com>
2024-03-12 20:20:19 +0200

    [frontend] Fix gql-codegen bad import

--- px-ui/src/gql/generated.ts
+++ px-ui/src/gql/generated.ts
@@ -1,7 +1,7 @@
 import { ReadStream } from 'fs';
 import { z } from 'zod'
 import { GraphQLClient } from 'graphql-request';
-import { RequestInit } from 'graphql-request/dist/types.dom';
+
 import { useMutation, useQuery, useInfiniteQuery, UseMutationOptions, UseQueryOptions, UseInfiniteQueryOptions } from '@tanstack/react-query';
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
